b35bafc42ead725069ee5b84756ddd69bd786659
onetimecircle <lerners124@gmail.com>
2023-07-31 19:09:52 -0400

    defined my schemas

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const CategorySchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+})
+
+
+CategorySchema.virtual('url').get(function () {
+    return `/home/category/${this.id}`
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
---
+++ models/item.js
@@ -0,0 +1,18 @@
+const mongoose =  require('mongoose')
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    category: [{type: Schema.Types.ObjectId, ref: "Category", required: true}],
+    price: {type: Number, required: true},
+    inStock: {type: Number, required: true}
+})
+
+
+ItemSchema.virtual('url').get(function () {
+    return `/home/item/${this.id}`
+})
+
+module.exports = mongoose.model("Item", ItemSchema)
