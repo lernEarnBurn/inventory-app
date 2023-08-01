efaf86ec11ea3d6cc7743360cb638f4e1d14fd30
onetimecircle <lerners124@gmail.com>
2023-07-31 21:03:14 -0400

    gonna implement images

--- models/item.js
+++ models/item.js
@@ -7,12 +7,17 @@ const ItemSchema = new Schema({
     description: {type: String, required: true},
     category: [{type: Schema.Types.ObjectId, ref: "Category", required: true}],
     price: {type: Number, required: true},
-    inStock: {type: Number, required: true}
+    inStock: {type: Number, required: true},
+    image: {type: Schema.Types.ObjectId, ref: "Image"}
 })
 
 
 ItemSchema.virtual('url').get(function () {
     return `/home/item/${this.id}`
+})
+
+ItemSchema.virtual('priceString').get(function () {
+    return `$${this.price.toString()}`
 })
 
 module.exports = mongoose.model("Item", ItemSchema)
